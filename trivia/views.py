9bbb032e0b33fdf68fda4eb45cd1bb8b9294851c
Danfeng "Danny" Wang <dannywang@utexas.edu>
2018-11-11 23:41:52 -0600

    almost ready

--- trivia/views.py
+++ trivia/views.py
@@ -7,25 +7,36 @@ from trivia.models import Category, Question, Choice
 
 
 class HomeListView(ListView):
-	model = Category
+
 	template_name = 'home.html'
+	
+	def get_queryset(self):
+		self.request.session['score'] = 0
+		return Category.objects.all()
 
 class QuestionDetailView(DetailView):
-	model = Question
+
 	template_name = 'question.html'
+
+	def get_queryset(self):
+		self.category = get_object_or_404(Category, name=self.kwargs['category_name'])
+		return Question.objects.filter(category=self.category)
 
 
 def check_answer(request, category_name, pk):
 	question = get_object_or_404(Question, pk=pk)
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+		if selected_choice.is_correct_answer == True:
+			request.session['score'] += 10
 	except (KeyError, Choice.DoesNotExist):
 		return render(request, 'question.html', {
 			'question': question,
 			'error_message': "You did not select a choice...",
 		})
 	else:
-		if Question.objects.filter(pk=pk+1).exists():
+		category = get_object_or_404(Category, name=category_name)
+		if Question.objects.filter(category=category, pk=pk+1).exists():
 			return HttpResponseRedirect(reverse('trivia:question_page', args=(category_name, pk+1,)))
 		else:
 			return HttpResponseRedirect(reverse('trivia:result'))
